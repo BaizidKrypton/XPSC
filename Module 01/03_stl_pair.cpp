1406036e2aa2472e2432ad84af3c80a4d92582ad
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2024-10-27 00:23:43 +0600

    Add files via upload

---
+++ Module 01/03_stl_pair.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    pair<string,int> p;
+    p.first="Baizid";
+    p.second=85;
+    p=make_pair("Krypton",77);
+    cout<<p.first<<" "<<p.second<<endl;
+
+    return 0;
+}
