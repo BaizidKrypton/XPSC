69c54aa942fbce5238428abf4fdd9b88d78c1bcd
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2024-11-21 23:57:21 +0600

    Add files via upload

---
+++ week04/day05/E_Jellyfish_and_Undertale.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b,n;
+        cin>>a>>b>>n;
+
+        vector<ll> v(n);
+        for(int i=0; i<n; i++)
+        {
+            cin>>v[i];
+        }
+
+        int max_time=b;
+
+        for(int i=0; i<n; i++)
+        {
+            max_time+=min(v[i],a-1);
+        }
+
+        cout<<max_time<<endl;
+    }
+
+    return 0;
+}
