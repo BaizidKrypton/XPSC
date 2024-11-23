a4decff2468564461d49bdc130b222d4d8a60d4b
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2024-11-23 23:53:08 +0600

    Add files via upload

---
+++ week04/day07/C_Assembly_via_Minimums.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int b_n=(n*(n-1))/2;
+        vector<int> v(b_n);
+        for(int i=0; i<b_n; i++)
+        {
+            cin>>v[i];
+        }
+
+        sort(v.begin(),v.end());
+
+        int idx=0;
+        for(int i=1; i<n; i++)
+        {
+            cout<<v[idx]<<" ";
+            idx+=(n-i);
+        }
+        int last=1e9;
+        cout<<last<<endl;
+    }
+
+    return 0;
+}
