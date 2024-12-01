4d0da414e16f3e98f4f4900d1634343201acdad5
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2024-12-01 23:20:49 +0600

    Add files via upload

---
+++ week06/day05/A_Bone_Appetit.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int bones,blood;
+    cin>>bones>>blood;
+
+    int x,y;
+    cin>>x>>y;
+
+    cout<<(bones*x)+(blood*y)<<endl;
+
+    return 0;
+}
