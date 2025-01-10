c33e7e1cf90817c112b097651d9ebff0aad1e670
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-01-11 01:10:22 +0600

    Update E_Building_an_Aquarium.cpp

--- week11/day05/E_Building_an_Aquarium.cpp
+++ week11/day05/E_Building_an_Aquarium.cpp
@@ -16,25 +16,30 @@ int main()
             cin>>v[i];
         }
 
+        auto ok=[&](int h)
+        {
+            ll res=0;
+            for(ll val:v)
+            {
+                if(h>val)
+                {
+                    res+=(h-val);
+                }
+            }
+            return res<=x;
+        };
+
         ll l=1, r=INT_MAX, mid, ans=INT_MAX;
         while(l<=r)
         {
             mid=(l+r)/2;
-            ll res=0;
-            for(ll val:v)
-            {
-                if(mid>val)
-                {
-                    res+=(mid-val);
-                }
-            }
-            if(res<=x)
+            if(ok(mid))
             {
                 ans=mid;
                 l=mid+1;
             }
             else
-            {
+            { 
                 r=mid-1;
             }
         }
