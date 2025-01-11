108214ee4ae0f03a2e11c2cc3633a9db72a2bccd
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-01-11 23:53:28 +0600

    Add files via upload

---
+++ week11/day06/F_Final_Boss.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll h,n;
+        cin>>h>>n;
+        vector<ll> a(n);
+        vector<ll> c(n);
+        for(int i=0; i<n; i++)
+        {
+            cin>>a[i];
+        }
+        for(int i=0; i<n; i++)
+        {
+            cin>>c[i];
+        }
+
+        auto ok=[&](ll x)
+        {
+            ll sum=0;
+            for(int i=0; i<n; i++)
+            {
+                ll z=(x-1ll)/c[i];
+                sum+=(z*a[i]);
+                if(sum>=h)
+                {
+                    break;
+                }
+            }
+            return sum>=h;
+        };
+
+        ll l=1, r=2e12, mid, ans=r;
+        while(l<=r)
+        {
+            mid=(l+r)/2ll;
+            if(ok(mid))
+            {
+                ans=min(ans,mid);
+                r=mid-1ll;
+            }
+            else
+            {
+                l=mid+1ll;
+            }
+        }
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
