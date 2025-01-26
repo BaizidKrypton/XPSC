7915e1c279b5e03498665b5d477779cefd98acb4
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-01-26 23:21:17 +0600

    Add files via upload

---
+++ week13/day06/C_Delete_Two_Elements.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        vector<ll> v(n);
+        ll sum=0;
+        for(int i=0; i<n; i++)
+        {
+            cin>>v[i];
+            sum+=v[i];
+        }
+
+        if((2*sum)%n)
+        {
+            cout<<"0"<<endl;
+            continue;
+        }
+
+        ll ans=0;
+        map<ll,ll> mp;
+        for(int i=0; i<n; i++)
+        {
+            ans+=mp[2*sum/n-v[i]];
+            mp[v[i]]++;
+        }
+
+        
+    }
+
+    return 0;
+}
