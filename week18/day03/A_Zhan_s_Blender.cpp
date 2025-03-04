67f3f755cf9b55b329357ae2417f60e48390e824
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-03-04 23:37:04 +0600

    Add files via upload

---
+++ week18/day03/A_Zhan_s_Blender.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,x,y;
+        cin>>n>>x>>y;
+
+        ll fill_time=(n+x-1)/x;
+        ll blend_time=(n+y-1)/y;
+
+        ll ans=min(fill_time,blend_time);
+
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
