6121b9e3799a44cb3981f6a72da9b6df377ff896
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-04-10 23:19:18 +0600

    Add files via upload

---
+++ week23/day05/A_Kamilka_and_the_Sheep.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        set<int> s;
+
+        for(int i=0; i<n; i++)
+        {
+            int x;
+            cin>>x;
+            s.insert(x);
+        }
+
+        auto ans=*s.rbegin()-*s.begin();
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
