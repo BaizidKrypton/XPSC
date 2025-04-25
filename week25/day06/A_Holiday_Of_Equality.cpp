f990e6ce5b9e2797540bf8ce791ca1dee16349af
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-04-25 22:17:10 +0600

    Add files via upload

---
+++ week25/day06/A_Holiday_Of_Equality.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+
+    int ans=0;
+    for(int i=0; i<n-1; i++)
+    {
+        ans+=v[n-1]-v[i];
+    }
+
+    cout<<ans<<endl;
+
+    return 0;
+}
