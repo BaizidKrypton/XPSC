b57645e3baa2f7d4b6e2b2ff36fc63b7aac55ed7
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-06-05 22:53:28 +0600

    Add files via upload

---
+++ week31/day05/A_Only_Pluses.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        vector<int> v(3);
+        for(int i=0; i<3; i++)
+        {
+            cin>>v[i];
+        }
+
+        for(int i=0; i<5; i++)
+        {
+            sort(v.begin(),v.end());
+            v[0]++;
+        }
+
+        cout<<v[0]*v[1]*v[2]<<endl;
+    }
+
+    return 0;
+}
