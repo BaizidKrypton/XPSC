e7230b484e7d5d39dab018645067f1bba572e83e
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-06-17 22:41:10 +0600

    Add files via upload

---
+++ week33/day03/A_Vlad_and_the_Best_of_Five.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        map<char,int> mp;
+
+        for(int i=0; i<s.size(); i++)
+        {
+            mp[s[i]]++;
+        }
+
+        if(mp['A']>mp['B'])
+        {
+            cout<<"A"<<endl;
+        }
+        else
+        {
+            cout<<"B"<<endl;
+        }
+    }
+
+    return 0;
+}
