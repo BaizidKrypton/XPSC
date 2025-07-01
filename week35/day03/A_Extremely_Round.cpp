827cbc36b95d7dd7c45eb832d8abdadb623388bd
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-07-01 22:44:25 +0600

    Add files via upload

---
+++ week35/day03/A_Extremely_Round.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int cnt=0;
+        for(int i=1; i<=n; i*=10)
+        {
+            for(int j=1; j<=9; j++)
+            {
+                if(j*i<=n)
+                {
+                    cnt++;
+                }
+            }
+        }
+        cout<<cnt<<endl;
+    }
+
+    return 0;
+}
