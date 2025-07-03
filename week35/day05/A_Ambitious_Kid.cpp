a90cfc1bc1cf53261c4355ed076b75566409d6cd
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-07-03 19:58:50 +0600

    Add files via upload

---
+++ week35/day05/A_Ambitious_Kid.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    }
+
+    int ans=INT_MAX;
+    for(int i=0; i<n; i++)
+    {
+        ans=min(ans,abs(v[i]-0));
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
