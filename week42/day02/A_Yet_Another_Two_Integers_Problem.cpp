0b0d2dcd0e540be419e204f1530f913af070b0f8
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-08-18 20:59:31 +0600

    Add files via upload

---
+++ week42/day02/A_Yet_Another_Two_Integers_Problem.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+
+        int x=abs(a-b);
+        if(x==0)
+        {
+            cout<<0<<endl;
+        }
+        else if(x>10)
+        {
+            if(x%10==0)
+            {
+                cout<<x/10<<endl;
+            }
+            else
+            {
+                cout<<(x/10)+1<<endl;
+            }
+        }
+        else
+        {
+            cout<<1<<endl;
+        }
+    }
+
+    return 0;
+}
