b1811c048b2674d3d7fe7f55a9155a03ba3a68a3
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-08-24 19:29:36 +0600

    Add files via upload

---
+++ week43/day01/A_EhAb_AnD_gCd.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int a=1;
+        int b=n-1;
+        cout<<a<<" "<<b<<endl;
+    }
+
+    return 0;
+}
