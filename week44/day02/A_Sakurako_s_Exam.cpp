a36d80aaa38524291ad4044e339613cecddd5c10
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-09-01 22:01:19 +0600

    Add files via upload

---
+++ week44/day02/A_Sakurako_s_Exam.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+
+        if(a==0 || b==0)
+        {
+            if(a==0 && b%2==0)
+            {   
+                cout<<"YES"<<endl;
+            }
+            else if(b==0 && a%2==0)
+            {
+                cout<<"YES"<<endl;
+            }
+            else
+            {
+                cout<<"NO"<<endl;
+            }
+        }
+        else
+        {
+            if(a%2==0 && b%2==0)
+            {
+                cout<<"YES"<<endl;
+            }
+            else if(a%2==0 && b%2!=0)
+            {
+                cout<<"YES"<<endl;
+            }
+            else
+            {
+                cout<<"NO"<<endl;
+            }
+        }        
+    }
+
+    return 0;
+}
