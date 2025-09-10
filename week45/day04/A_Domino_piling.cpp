2cf58ae316581d5f8a54275967dfd6de67ad2c75
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-09-10 22:23:32 +0600

    Add files via upload

---
+++ week45/day04/A_Domino_piling.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+
+    int mx=m*n;
+    int ans=0;
+    int cnt=0;
+    while(ans<mx)
+    {
+       
+        ans+=(2*1);
+        if(ans<=mx)
+        {
+            cnt++;
+        }
+    }
+    cout<<cnt<<endl;
+
+    return 0;
+}
