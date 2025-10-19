e0617693c7ac62e306cc2c00c143b731789a769b
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-10-19 18:36:17 +0600

    Add files via upload

---
+++ week51/day01/A_Beautiful_Average.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+
+        for(int i=0; i<n; i++)
+        {
+            cin>>v[i];
+        }
+
+        auto ans=max_element(v.begin(),v.end());
+        cout<<*ans<<endl;
+    }
+
+    return 0;
+}
