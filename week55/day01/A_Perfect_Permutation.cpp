4c75b8b72e2c5fee22b3c54d468b01be87292fbf
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-11-16 20:11:49 +0600

    Add files via upload

---
+++ week55/day01/A_Perfect_Permutation.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        if(n==1)
+        {
+            cout<<1<<endl;
+        }
+        else if(n==2)
+        {
+            cout<<2<<" "<<1<<endl;
+        }
+        else
+        {
+            cout<<n<<" ";
+            for(int i=1; i<n-1; i++)
+            {
+                cout<<i<<" ";
+            }
+            cout<<n-1<<endl;
+        }
+    }
+
+    return 0;
+}
