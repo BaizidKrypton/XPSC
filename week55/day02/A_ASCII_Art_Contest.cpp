eea6ac137ef91568a521063fbc67837bf6fbf26a
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-11-17 20:46:46 +0600

    Add files via upload

---
+++ week55/day02/A_ASCII_Art_Contest.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v(3);
+
+    for(int i=0; i<3; i++)
+    {
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+
+    if((v[2]-v[0])>=10)
+    {
+        cout<<"check again"<<endl;
+    }
+    else
+    {
+        cout<<"final"<<" "<<v[1]<<endl;
+    }
+
+    return 0;
+}
