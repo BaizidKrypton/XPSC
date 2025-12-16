af877c0f0076dbaa5d20a3da5fc66d2a9421587f
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2025-12-16 22:01:06 +0600

    Add files via upload

---
+++ week59/day03/A_Digit_Minimization.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+
+        if(s.size()==2)
+        {
+            cout<<s[s.size()-1]<<endl;
+        }
+        else
+        {
+            sort(s.begin(),s.end());
+            cout<<s[0]<<endl;
+        }
+    }
+
+    return 0;
+}
