f2207de1d8d44e6c8939ac4467900b04039efd16
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2026-01-04 20:55:20 +0600

    Add files via upload

---
+++ week62/day01/A_Park_Lighting.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        int x=n*m;
+
+        if(x%2==0)
+        {
+            cout<<x/2<<endl;
+        }
+        else
+        {
+            cout<<(x/2)+1<<endl;
+        }
+    }
+
+    return 0;
+}
