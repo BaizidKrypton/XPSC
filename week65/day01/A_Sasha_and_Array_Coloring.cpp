5ec9666918df5df3d435f0da41eae9f418eeb3f0
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2026-01-25 22:14:00 +0600

    Add files via upload

---
+++ week65/day01/A_Sasha_and_Array_Coloring.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        
+        sort(v.begin(), v.end());
+        
+        int ans = 0;
+        for (int i = 0; i < n/2; i++) {
+            ans += v[n - 1 - i] - v[i];
+        }
+        
+        cout << ans << endl;
+    }
+    return 0;
+}
