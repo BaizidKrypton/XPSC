c518aad202b22b9dd441778313f620d50762cfd9
Baizid Biswas <128794836+BaizidKrypton@users.noreply.github.com>
2026-02-23 02:07:51 +0600

    Add files via upload

---
+++ week69/day01/A_CQXYM_Count_Permutations.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        n+=n;
+ 
+        long long fact=1;
+        for(int i=3; i<=n; i++)
+        {
+            fact*=i;
+            fact%=mod;
+        }
+        cout<<fact<<endl;
+    }
+    return 0;
+}
